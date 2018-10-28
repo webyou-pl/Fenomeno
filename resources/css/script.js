63f03b0b5fe8196e7b603295cd61a413aaaef336
Mazur <and.mazur5@gmail.com>
2018-10-28 17:43:28 +0100

    Newest

--- resources/css/script.js
+++ resources/css/script.js
@@ -11,12 +11,12 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
+
   var slides = document.getElementsByClassName("mySlides");
 
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
     slideIndex++;
