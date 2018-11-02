620bedce20acc4d328026010fd01b7b102e1582c
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-02 22:12:48 +0100

    common layout php

--- index.php
+++ index.php
@@ -1,72 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <meta name="description" content="">
-        <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
-        <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
-        <link rel="stylesheet" type="text/css" href="vendors/css/animations.css">
-        <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
-        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
-        <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
-    <title>Fenomeno</title>
-</head>
-<body>
-    <script src="vendors/js/jquery-1.11.3.min.js" type="text/javascript"></script>
-    <script src="vendors/js/jssor.slider-27.5.0.min.js" type="text/javascript"></script>
-    <script src="vendors/js/min_slider_partners.js" type="text/javascript"></script>
-<!-- Start top heder -->
-<header>
-    <section>
-        <div class="top_header">
-            <div class="lan_con">
-                <div class="languages">
-                    <div>
-                        <a href="#!"> 
-                            <img src="https://png.icons8.com/color/24/000000/poland.png">
-                        </a>
-                        <a href="#!">
-                            <img src="https://png.icons8.com/color/24/000000/great-britain.png">
-                        </a>
-                    </div>
-                </div>
-                <div class="contact">
-                    <div>
-                        <div class="tel"><a href="#!"> Tel: 123 156 753</a></div>
-                        <div class="email"><a href="#!">Email: adres@domena.pl</a></div>
-                    </div>
-                </div>
-            </div>
-            <div class="logo_socjal">
-                <div class="logo">
-                    <a href="#!"><img class="logo_img" src="resources/css/img/logo_fenomeno_wroclaw.png" alt="logo"/></a> 
-                    <h1 class="logo_text"><a href="#!">fenomeno <span class="color_awarded_one">wro</span>cław</a></h1>
-                </div>
-                <div class="socjal">
-                    <a href="#!"><img src="https://png.icons8.com/color/32/000000/facebook.png"></a>
-                    <a href="#!"><img src="https://png.icons8.com/color/32/000000/instagram-new.png"></a>
-                    <a href="#!"><img src="https://png.icons8.com/color/32/000000/youtube-play.png"></a>
-                </div>
-            </div>
-            <!-- start main navigation -->
-            <nav>
-                <div class="row">
-                    <ul class="main-nav">
-                        <li><a href="#">O szkole</a></li>
-                        <li><a href="#">Oferta</a></li>
-                        <li><a href="#">Kadra</a></li>
-                        <li><a href="#">Galeria</a></li>
-                        <li><a href="#">Kontakt</a></li> 
-                    </ul>
-                </div> 
-            </nav>
-            <!-- End main navigation -->
-        </div>
-    </section>
-</header>
-<!-- END top heder -->
+<?php
+require_once 'structure/template.php';
+?>
+<?php echo $template_start; ?>
 <!-- Slideshow container -->
 <section class="main">
     <div class="slideshow-container">
@@ -167,9 +102,7 @@
             </div>
         </div>
     </div>
-    <div class="footer">
-            ©2018 /localhost/ - wszelkie prawa zastrzeżone. Projekt zrealizowany przez zespół webyou.
-    </div>
+    <?php echo $template_footer; ?>
 </footer>
 <!-- end footer -->
 </body>
