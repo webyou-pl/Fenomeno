d47f814eb2638d32e33a06b69f0ad77efddfa412
Mazur <and.mazur5@gmail.com>
2018-11-05 22:35:04 +0100

    Removed counter animation - added dots to see slide

--- index.php
+++ index.php
@@ -58,6 +58,14 @@ require_once 'structure/template.php';
         </div>
     </div>
 </section>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
     <!-- END Slideshow container -->
 <!-- start footer -->
 <footer class="down_footer">
--- resources/css/script.js
+++ resources/css/script.js
@@ -1,26 +1,32 @@
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-
+  var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (var i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 6000); // Change image every x seconds
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+ 
 }
+
+
+
+   
