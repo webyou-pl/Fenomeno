f206bc2bde6a4195dafb298465e442fde4e0d61b
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-06 23:01:02 +0100

    starting styling the contact

--- kontakt.php
+++ kontakt.php
@@ -2,76 +2,25 @@
 require_once 'structure/template.php';
 ?>
 <?php echo $template_start; ?>
-        <section class="oferta">
-             <div class="row">
-            <h4 class="text-oferta">Zacznij trening dzisiaj</h4>
-                 <hr>
-                
+        <section class="contact">
+            <div class="row">
+                <!-- klasa powina byc ogolna -->
+                <h4 class="text-oferta">test</h4>
+                <hr/>
             </div>
-        <div class="row">
-            <div class="col span-1-of-3">
-                <div class="plan-box js--wp-4">
-                    <div>
-                        <h3>PErsonal Training</h3>
-                        <p class="plan-price">krotki opis</p>
-                    </div>
-                    <div class="skrzynka">
-                        <ul>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="#" class="btn btn-ghost">Contact now</a>
-                    </div>
-                </div>
+            <div class="container">
+                <div class="contact__header"><img src="resources/css/img/logo_fenomeno_wroclaw.png"><span class="logo__title">Fenomeno Wroclaw</span>
+            ul. nazwa 123/1<br />
+            wrocław 51-109<br />
+            tel. 213 123 532<br />
+            email: adres@domena.pl<br />
             </div>
- 
-
-            <div class="col span-1-of-3">
-                <div class="plan-box js--wp-4">
-                    <div>
-                        <h3>PErsonal Training</h3>
-                        <p class="plan-price">krotki opis</p>
-                    </div>
-                    <div class="skrzynka">
-                        <ul>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="#" class="btn btn-full">Contact now</a>
-                    </div>
-                </div>
+                <div class="contact__map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.5834064413784!2d17.029909816096826!3d51.134591879575645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9933eecb78d%3A0xb7fecab74d0ee746!2sUnnamed+Road%2C+50-001+Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1541541169100" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+                <div class="contact__form">formularz</div>
             </div>
-  
-            <div class="col span-1-of-3">
-                <div class="plan-box js--wp-4">
-                    <div>
-                        <h3>PErsonal Training</h3>
-                        <p class="plan-price">krotki opis</p>
-                    </div>
-                    <div class="skrzynka">
-                        <ul>
-                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small skrzynka"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="#" class="btn btn-ghost">Contact now</a>
-                    </div>
-                </div>
-            </div> 
-        </div>
-    </section>
+            </div>
+        </section>
     <?php echo $template_footer; ?>
 </body>
-    <script src="resources/css/script.js"></script>
+
 </html>
