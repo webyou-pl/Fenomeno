e9ca55f4e7b6b081452fe6b43c3669cd2eb8ad89
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-06 21:38:19 +0100

    adding contact and img background

---
+++ kontakt.php
@@ -0,0 +1,77 @@
+<?php
+require_once 'structure/template.php';
+?>
+<?php echo $template_start; ?>
+        <section class="oferta">
+             <div class="row">
+            <h4 class="text-oferta">Zacznij trening dzisiaj</h4>
+                 <hr>
+                
+            </div>
+        <div class="row">
+            <div class="col span-1-of-3">
+                <div class="plan-box js--wp-4">
+                    <div>
+                        <h3>PErsonal Training</h3>
+                        <p class="plan-price">krotki opis</p>
+                    </div>
+                    <div class="skrzynka">
+                        <ul>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-ghost">Contact now</a>
+                    </div>
+                </div>
+            </div>
+ 
+
+            <div class="col span-1-of-3">
+                <div class="plan-box js--wp-4">
+                    <div>
+                        <h3>PErsonal Training</h3>
+                        <p class="plan-price">krotki opis</p>
+                    </div>
+                    <div class="skrzynka">
+                        <ul>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-full">Contact now</a>
+                    </div>
+                </div>
+            </div>
+  
+            <div class="col span-1-of-3">
+                <div class="plan-box js--wp-4">
+                    <div>
+                        <h3>PErsonal Training</h3>
+                        <p class="plan-price">krotki opis</p>
+                    </div>
+                    <div class="skrzynka">
+                        <ul>
+                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small skrzynka"></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
+                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-ghost">Contact now</a>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </section>
+    <?php echo $template_footer; ?>
+</body>
+    <script src="resources/css/script.js"></script>
+</html>
