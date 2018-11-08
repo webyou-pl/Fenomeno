4b189dad664b82fbd176c86866281604453a18ab
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-08 21:11:11 +0100

    edit maps google

--- kontakt.php
+++ kontakt.php
@@ -17,7 +17,7 @@ require_once 'structure/template.php';
                     tel. 213 123 532<br />
                     email: adres@domena.pl<br />
                 </div>
-                <div class="contact__map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.5834064413784!2d17.029909816096826!3d51.134591879575645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9933eecb78d%3A0xb7fecab74d0ee746!2sUnnamed+Road%2C+50-001+Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1541541169100" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+                <div class="contact__map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.5834064413784!2d17.029909816096826!3d51.134591879575645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9933eecb78d%3A0xb7fecab74d0ee746!2sUnnamed+Road%2C+50-001+Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1541541169100" frameborder="0" style="border:0" allowfullscreen></iframe></div>
                 <div class="contact__form">
                     <form action="none">
                         <p>Twój Email:</p> 
@@ -25,7 +25,7 @@ require_once 'structure/template.php';
                         <p class="e--form">e_email</p>
 
                         <p>Treść Wiadomości:</p>
-                        <textarea name="message" rows="10" cols="30" > ?? </textarea>
+                        <textarea name="message" rows="10" cols="30" >  </textarea>
                         <p class="e--form">e_message</p>
                         <input type="submit" value="WYŚLIJ">
                     </form>
