774b446ae46f9b88272ba16ac6df519b1bbed22f
Mazur <and.mazur5@gmail.com>
2018-11-11 20:39:12 +0100

    image optimalization

--- galeria.php
+++ galeria.php
@@ -15,26 +15,27 @@ require_once 'structure/template.php';
         <!-- Big Image -->
     
         <div class="gallery">
-            <a href="Material/Galeria/1.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/1.jpg"></a>
-            <a href="Material/Galeria/2.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/2.jpg"></a>
-            <a href="Material/Galeria/3.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/3.jpg"></a>
-            <a href="Material/Galeria/4.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/4.jpg"></a>
-            <a href="Material/Galeria/5.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/5.jpg"></a>
-            <a href="Material/Galeria/6.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/6.jpg"></a>
-            <a href="Material/Galeria/7.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/7.jpg"></a>
-            <a href="Material/Galeria/8.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/8.jpg"></a>
-            <a href="Material/Galeria/9.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/9.jpg"></a>
-            <a href="Material/Galeria/10.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/10.jpg"></a>
-            <a href="Material/Galeria/11.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/11.jpg"></a>
-            <a href="Material/Galeria/12.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/12.jpg"></a>
-            <a href="Material/Galeria/1.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/1.jpg"></a>
-            <a href="Material/Galeria/2.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/2.jpg"></a>
-            <a href="Material/Galeria/3.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/3.jpg"></a>
-            <a href="Material/Galeria/4.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/4.jpg"></a>
-            <a href="Material/Galeria/5.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/5.jpg"></a>
-            <a href="Material/Galeria/6.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/6.jpg"></a>
+            <a href="Material/Galeria/1.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/1-small.jpg"></a>
+            <a href="Material/Galeria/2.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/2-small.jpg"></a>
+            <a href="Material/Galeria/3.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/3-small.jpg"></a>
+            <a href="Material/Galeria/4.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/4-small.jpg"></a>
+            <a href="Material/Galeria/5.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/5-small.jpg"></a>
+            <a href="Material/Galeria/6.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/6-small.jpg"></a>
+            <a href="Material/Galeria/7.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/7-small.jpg"></a>
+            <a href="Material/Galeria/8.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/8-small.jpg"></a>
+            <a href="Material/Galeria/9.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/9-small.jpg"></a>
+            <a href="Material/Galeria/10.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/10-small.jpg"></a>
+            <a href="Material/Galeria/11.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/11-small.jpg"></a>
+            <a href="Material/Galeria/12.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/12-small.jpg"></a>
+            <a href="Material/Galeria/1.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/1-small.jpg"></a>
+            <a href="Material/Galeria/2.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/2-small.jpg"></a>
+            <a href="Material/Galeria/3.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/3-small.jpg"></a>
+            <a href="Material/Galeria/4.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/4-small.jpg"></a>
+            <a href="Material/Galeria/5.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/5-small.jpg"></a>
+            <a href="Material/Galeria/6.jpg" data-lightbox="mygallery"> <img src="Material/Galeria/6-small.jpg"></a>
         </div>
 
-    
+
 </section>
     <script type="text/javascript" src="vendors/js/lightbox-plus-jquery.min.js"></script>
+  <?php echo $template_footer; ?>  
