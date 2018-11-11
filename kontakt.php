039d87899ab2cd153e74160cb6a2a50eedc7914c
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2018-11-11 22:33:22 +0100

    reduction map google

--- kontakt.php
+++ kontakt.php
@@ -24,14 +24,14 @@ require_once 'structure/template.php';
                             <p class="e--form">e_email</p>
                             
                             <p>Treść Wiadomości:</p>
-                            <textarea name="message" rows="10" cols="30" >  </textarea>
+                            <textarea name="message" rows="5" cols="30" >  </textarea>
                             <p class="e--form">e_message</p>
                             <input type="submit" value="WYŚLIJ">
                         </form>
                     </div>
                     <div class="contact__map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d882.607892082422!2d17.03120408378203!3d51.12796093735983!2m3!1f0!2f39.2014487898321!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x470fe99348801cd5%3A0x97bcba10fc9d97fc!2sLiceum+Og%C3%B3lnokszta%C5%82c%C4%85ce+nr+X+im!5e1!3m2!1spl!2spl!4v1541714862880"  frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.6114029937175!2d17.03148589469973!3d51.13407552396919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x97bcba10fc9d97fc!2sLiceum+Og%C3%B3lnokszta%C5%82c%C4%85ce+nr+X+im!5e0!3m2!1spl!2spl!4v1541971639925" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
                         
                 </div>
             </div>
