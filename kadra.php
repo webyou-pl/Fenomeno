9d30f407854b527259757c93b3be9c4d8a08853c
Mazur <and.mazur5@gmail.com>
2018-11-15 22:11:14 +0100

    Pop out added! Need to be edited in css now!

--- kadra.php
+++ kadra.php
@@ -14,38 +14,82 @@ require_once 'structure/template.php';
     <div class="row">
         <div class="trener1">
         <div class="col span-1-of-3 box">
-            <img src="Material/kadra/bryan%20west.jpg">
+        <a href="#" class="big-link" data-reveal-id="myModal">    
+            <img src="Material/kadra/bryan%20west.jpg"></a>
             <p>Imie Nazwisko</p>
         </div>
+        <div id="myModal" class="reveal-modal">
+            <h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a> 
+		      </div>   
+            
+            
         <div class="col span-1-of-3 box">
-            <img src="Material/kadra/donald%20evans.jpg">
+            <a href="#" class="big-link" data-reveal-id="myModal">
+            <img src="Material/kadra/donald%20evans.jpg"></a>
             <p>Imie Nazwisko</p>
             </div>
+                    <div id="myModal" class="reveal-modal">
+            <h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a> 
+		      </div>  
+            
+            
             <div class="col span-1-of-3 box">
-            <img src="Material/kadra/eva%20wade.jpg">
+            <a href="#" class="big-link" data-reveal-id="myModal">
+            <img src="Material/kadra/eva%20wade.jpg"></a>
                 <p>Imie Nazwisko</p>
             </div>
+                                <div id="myModal" class="reveal-modal">
+            <h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a> 
+		      </div>  
         </div>
+        
+        
         <div class="trener2">
             <div class="col span-1-of-2 box">
-            <img src="Material/kadra/guy%20ferguson.jpg">
+            <a href="#" class="big-link" data-reveal-id="myModal">
+            <img src="Material/kadra/guy%20ferguson.jpg"></a>
                 <p>Imie Nazwisko</p>
             </div>
+            <div id="myModal" class="reveal-modal">
+			<h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a> 
+		      </div>
+            
+            <!-- Way to make pop up! start -->
             <div class="col span-1-of-2 box">
-            <img src="Material/kadra/sherry%20lee.jpg">
+            <a href="#" class="big-link" data-reveal-id="myModal">
+            <img src="Material/kadra/sherry%20lee.jpg"></a>
             <p>Imie Nazwisko</p>
             </div>
+        
+            <div id="myModal" class="reveal-modal">
+			<h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a> 
+		      </div>
+            <!-- Way to make pop up! END -->
         </div>
-        </div>
-    
     </div>
     
+    
+    
+    
+
+
     
     
 </section>
 <scrip src="vendors/js/jquery.reveal.js"></scrip>
 </body>
+<script type="text/javascript" src="vendors/js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="vendors/js/jquery.reveal.js"></script>
 <?php echo $template_footer; ?>
 
 
-id="myModal" 
---
+++ test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <meta name="description" content="">
+        <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/animations.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+    <link rel="stylesheet" href="vendors/css/reveal.css">
+
+    
+    <title>Fenomeno</title>
+</head>
+<body>
+
+
+
+		<a href="#" class="big-link" data-reveal-id="myModal">
+			Fade and Pop
+		</a>	
+		
+		<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
+			Fade
+		</a>
+		
+		<a href="#" class="big-link" data-reveal-id="myModal" data-animation="none">
+			None
+		</a>
+
+		<div id="myModal" class="reveal-modal">
+			<h1>Reveal Modal Goodness</h1>
+			<p>This is a default modal in all its glory, but any of the styles here can easily be changed in the CSS.</p>
+			<a class="close-reveal-modal">&#215;</a>
+		</div>
+			
+
+
+
+    </body>
+    
+    <script type="text/javascript" src="vendors/js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="vendors/js/jquery.reveal.js"></script>
+</html>
