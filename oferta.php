80b63dc0f7c67c8b9bedd1a2d63dc30322199ddd
Mazur <and.mazur5@gmail.com>
2018-11-29 23:24:43 +0100

    Oferta_poprawiona_tabela zrobiona

--- oferta.php
+++ oferta.php
@@ -4,74 +4,106 @@ require_once 'structure/template.php';
 <?php echo $template_start; ?>
         <section class="oferta">
              <div class="row">
-            <h4 class="text">Zacznij trening dzisiaj</h4>
+            <h4 class="text">Oferta</h4>
                  <hr>
                 
             </div>
         <div class="row">
-            <div class="col span-1-of-3">
-                <div class="plan-box js--wp-4">
-                    <div>
-                        <h3 class="oferta-title">PErsonal Training</h3>
-                        <p class="plan-price">krotki opis</p>
-                    </div>
-                    <div class="skrzynka">
-                        <ul>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="#" class="btn btn-ghost">Contact now</a>
-                    </div>
-                </div>
-            </div>
- 
+            
+            
+        <table class="offer__table">
+            <tr>
+                <th>Grupa</th>
+                <th>Wiek</th>
+                <th>Ilość treningów</th>
+                <th>Karnet miesięczny</th>
+            </tr> 
+            
+            <tr>
+                <th>Bambini</th>
+                <td>0-3</td>
+                <td>1</td>
+                <td>15 zł zajęcia</td>    
+            </tr>
+            
+            <tr>
+                <th>Skrzaty</th>
+                <td>4-6</td>
+                <td>2-3</td>
+                <td>120 zł</td>    
+            </tr>
+            
+            <tr>
+                <th>Żaki</th>
+                <td>7-8</td>
+                <td>2-3</td>
+                <td>120 zł</td>    
+            </tr>
+            
+            <tr>
+                <th>Orliki</th>
+                <td>9-10</td>
+                <td>2-3 + zajęcia dodatkowe*</td>
+                <td>120/150 zł</td>    
+            </tr> 
 
-            <div class="col span-1-of-3">
-                <div class="plan-box js--wp-4">
-                    <div>
-                        <h3 class="oferta-title">PErsonal Training</h3>
-                        <p class="plan-price">krotki opis</p>
-                    </div>
-                    <div class="skrzynka">
-                        <ul>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small"></i>co zawiera</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="#" class="btn btn-full">Contact now</a>
-                    </div>
-                </div>
-            </div>
-  
-            <div class="col span-1-of-3">
-                <div class="plan-box js--wp-4">
-                    <div>
-                        <h3 class="oferta-title">PErsonal Training</h3>
-                        <p class="plan-price">krotki opis</p>
-                    </div>
-                    <div class="skrzynka">
-                        <ul>
-                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small skrzynka"></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
-                        <li><i class="ion-ios-checkmark-empty icon-small "></i>co zawiera</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="#" class="btn btn-ghost">Contact now</a>
-                    </div>
-                </div>
-            </div> 
+            <tr>
+                <th>Młodziki</th>
+                <td>11-12</td>
+                <td>3 + zajęcia dodatkowe*</td>
+                <td>120/150 zł</td>    
+            </tr>
+            
+            <tr>
+                <th>Tramkarze</th>
+                <td>13-14</td>
+                <td>3-4 + mecz</td>
+                <td>150 zł</td>    
+            </tr>
+            
+            <tr>
+                <th>Junior Młodszy</th>
+                <td>15-16</td>
+                <td>4-8 + mecz</td>
+                <td>Reprezentacja akademii</td>    
+            </tr>
+            
+            <tr>
+                <th>Junior Starszy</th>
+                <td>17-18</td>
+                <td>4-8 + mecz</td>
+                <td>Reprezentacja akademii</td>    
+            
+                <tr>     
+                <th colspan="4" class="offer__table-bottomBox">Zajęcia dodatkowe* - jest to zbiór wszelkich form aktywności dostosowanych do danej grupy treningowej. W ofercie zajęć młodzi piłkarze będą korzystać z lekcji pływania, zajęć lekkoatletycznych, zajęć gimnastycznych oraz sztuk walki (judo, taekwondo, karate).</th>
+                </tr>  
+            
+        </table>  
+            
+                <footer>
+               <a href="kontakt.php" class="btn btn-full">Skontaktuj się z nami</a>
+                </footer>
+
+
+                        
+                        
+
+                        
+                        
+               
+        </div>
+                        
+                        
+           
+
         </div>
     </section>
+<!--
+
+-->
+
+
     <?php echo $template_footer; ?>
 </body>
-    <script src="resources/css/script.js"></script>
+
 </html>
