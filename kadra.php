3372be8f533b7cd49ea9b69fcf8e0ce73984dcc6
Mazur <and.mazur5@gmail.com>
2018-12-02 00:44:49 +0100

    third cahnge need to add image

--- kadra.php
+++ kadra.php
@@ -13,17 +13,19 @@ require_once 'structure/template.php';
                 <hr/>
             </div>
     <div class="row">
-        <div class="trener1">
+        <div class="trener-firstLine">
         <div class="col span-1-of-3 box">
             
         <a href="#" class="big-link" data-reveal-id="myModal1">    
             <img class="trainer-pictureSmall" src="resources/img/kadra/bryan%20west.jpg"></a>
-            <p class="kadra--mainSectionNames">Imie Nazwisko</p>
+            <p class="kadra--mainSectionNames">Francisco Duarte</p>
         </div>
         <div id="myModal1" class="reveal-modal">
-            <h6>Imie Nazwisko</h6>
-            <img src="resources/img/kadra/trainer%20example.jpg" class="trainer-pictureBox">
-			<p class="trener_text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. TEXT1</p>
+            <h6>Francisco Duarte</h6>
+            <img src="resources/img/kadra/" class="trainer-pictureBox">
+			<p class="trener_text">Urodzony 04.03.1989r w Lizbonie. Ukończył FMH Lisboa na kierunku menadżera sportu, a poza prestiżową uczelnią zdobywał wiedzę i doświadczenie w takich klubach jak: Futebol Clube de Alverca, Uniao Atletico Povoense, Sport Lisboa e Benfica, Os Beleneses, Sporting Clube de Portugal, Empoli Football Club, Devexplorer Football Club, Winning League Figo Academy.</p>
+            <p class="trener_text">Przez ostatnie 4 lata pracował jako koordynator i główny trener w akademii Luisa Figo w Chinach. Do jego ciekawej historii trzeba dopisać doświadczenie jako łowca talentów do Benfici i Sportingu w Lizbonie.</p>
+            <p class="trener_text">Dziś z wielki aspiracjami wraca do Europy by uczyć nie tylko naszych podopiecznych, ale i trenerów akademii.</p>
 			<a class="close-reveal-modal">&#215;</a> 
 		      </div>   
             
@@ -55,7 +57,7 @@ require_once 'structure/template.php';
         </div>
         
         
-        <div class="trener2">
+        <div class="trener-secondLine">
             <div class="col span-1-of-2 box">
             <a href="#" class="big-link" data-reveal-id="myModal4">
             <img class="trainer-pictureSmall" src="resources/img/kadra/guy%20ferguson.jpg"></a>
