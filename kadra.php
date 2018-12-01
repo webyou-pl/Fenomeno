5948a94947b7fa370fb904adfbdc81dc5c8848c4
Mazur <and.mazur5@gmail.com>
2018-12-01 16:00:29 +0100

    first changes

--- kadra.php
+++ kadra.php
@@ -6,7 +6,7 @@ require_once 'structure/template.php';
 
 <?php echo $template_start; ?>
 <body>
-<section class="kadra">
+<section class="kadra--mainSection">
             <div class="row">
                 <!-- klasa powina byc ogolna -->
                 <h4 class="text">Kontakt</h4>
@@ -18,11 +18,11 @@ require_once 'structure/template.php';
             
         <a href="#" class="big-link" data-reveal-id="myModal1">    
             <img src="resources/img/kadra/bryan%20west.jpg"></a>
-            <p>Imie Nazwisko</p>
+            <p class="kadra--mainSectionNames">Imie Nazwisko</p>
         </div>
         <div id="myModal1" class="reveal-modal">
             <h6>Imie Nazwisko</h6>
-            <img src="resources/img/kadra/trainer%20example.jpg" class="trainer-picture">
+            <img src="resources/img/kadra/trainer%20example.jpg" class="trainer-pictureBox">
 			<p class="trener">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. TEXT1</p>
 			<a class="close-reveal-modal">&#215;</a> 
 		      </div>   
@@ -31,7 +31,7 @@ require_once 'structure/template.php';
         <div class="col span-1-of-3 box">
             <a href="#" class="big-link" data-reveal-id="myModal2">
             <img src="resources/img/kadra/donald%20evans.jpg"></a>
-            <p>Imie Nazwisko</p>
+            <p class="kadra--mainSectionNames">Imie Nazwisko</p>
             </div>
             <div id="myModal2" class="reveal-modal">
             <h6>Imie Nazwisko</h6>
@@ -44,7 +44,7 @@ require_once 'structure/template.php';
             <div class="col span-1-of-3 box">
             <a href="#" class="big-link" data-reveal-id="myModal3">
             <img src="resources/img/kadra/eva%20wade.jpg"></a>
-                <p>Imie Nazwisko</p>
+                <p class="kadra--mainSectionNames">Imie Nazwisko</p>
             </div>
             <div id="myModal3" class="reveal-modal">
             <h6>Imie Nazwisko</h6>
@@ -59,7 +59,7 @@ require_once 'structure/template.php';
             <div class="col span-1-of-2 box">
             <a href="#" class="big-link" data-reveal-id="myModal4">
             <img src="resources/img/kadra/guy%20ferguson.jpg"></a>
-                <p>Imie Nazwisko</p>
+                <p class="kadra--mainSectionNames">Imie Nazwisko</p>
             </div>
             <div id="myModal4" class="reveal-modal">
             <h6>Imie Nazwisko</h6>
@@ -72,7 +72,7 @@ require_once 'structure/template.php';
             <div class="col span-1-of-2 box">
             <a href="#" class="big-link" data-reveal-id="myModal5">
             <img src="resources/img/kadra/sherry%20lee.jpg"></a>
-            <p>Imie Nazwisko</p>
+            <p class="kadra--mainSectionNames">Imie Nazwisko</p>
             </div>
         
             <div id="myModal5" class="reveal-modal">
