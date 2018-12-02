97be3f44d284c7f532ea88417b718a6c4e5860aa
Mazur <and.mazur5@gmail.com>
2018-12-02 11:56:18 +0100

    need to find element.style -> cause cannot move pop up window up!

--- kadra.php
+++ kadra.php
@@ -17,12 +17,12 @@ require_once 'structure/template.php';
         <div class="col span-1-of-3 box">
             
         <a href="#" class="big-link" data-reveal-id="myModal1">    
-            <img class="trainer-pictureSmall" src="resources/img/kadra/bryan%20west.jpg"></a>
+            <img class="trainer-pictureSmall" src="resources/img/kadra/Francisco%20Duarte_small.jpg"></a>
             <p class="kadra--mainSectionNames">Francisco Duarte</p>
         </div>
         <div id="myModal1" class="reveal-modal">
             <h6>Francisco Duarte</h6>
-            <img src="resources/img/kadra/" class="trainer-pictureBox">
+            <img src="resources/img/kadra/Francisco%20Duarte_big.jpg" class="trainer-pictureBox">
 			<p class="trener_text">Urodzony 04.03.1989r w Lizbonie. Ukończył FMH Lisboa na kierunku menadżera sportu, a poza prestiżową uczelnią zdobywał wiedzę i doświadczenie w takich klubach jak: Futebol Clube de Alverca, Uniao Atletico Povoense, Sport Lisboa e Benfica, Os Beleneses, Sporting Clube de Portugal, Empoli Football Club, Devexplorer Football Club, Winning League Figo Academy.</p>
             <p class="trener_text">Przez ostatnie 4 lata pracował jako koordynator i główny trener w akademii Luisa Figo w Chinach. Do jego ciekawej historii trzeba dopisać doświadczenie jako łowca talentów do Benfici i Sportingu w Lizbonie.</p>
             <p class="trener_text">Dziś z wielki aspiracjami wraca do Europy by uczyć nie tylko naszych podopiecznych, ale i trenerów akademii.</p>
